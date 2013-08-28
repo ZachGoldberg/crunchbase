cc1325f394d58810d81b3623d8c595d484831fd0
Zach Goldberg <zach@zachgoldberg.com>
2013-08-28 12:31:35 -0700

    More PEP8

--- crunchbase.py
+++ crunchbase.py
@@ -16,87 +16,87 @@ API_URL = API_BASE_URL + "v" + "/" + API_VERSION + "/"
 
 class CrunchbaseAPI(object):
     def __webRequest(self, url):
-      try:
-        response = urllib2.urlopen(url)
-        result = response.read()
-        return result
-      except urllib2.HTTPError as e:
-        raise CrunchBaseError(e)
+        try:
+            response = urllib2.urlopen(url)
+            result = response.read()
+            return result
+        except urllib2.HTTPError as e:
+            raise CrunchBaseError(e)
 
     def __getJsonData(self, namespace, query=""):
-      url = API_URL + namespace + query + ".js"
-      response_dict = json.loads(self.__webRequest(url))
-      return CrunchBaseResponse(**response_dict)
+        url = API_URL + namespace + query + ".js"
+        response_dict = json.loads(self.__webRequest(url))
+        return CrunchBaseResponse(**response_dict)
 
     def getCompanyData(self, name):
-      '''This returns the data about a company in JSON format.'''
+        '''This returns the data about a company in JSON format.'''
 
-      result = self.__getJsonData("company", "/%s" % name)
-      return result
+        result = self.__getJsonData("company", "/%s" % name)
+        return result
 
     def getPersonData(self, *args):
-      '''This returns the data about a person in JSON format.'''
+        '''This returns the data about a person in JSON format.'''
 
-      result = self.__getJsonData("person", "/%s" % '-'.join(
-          args).lower().replace(' ', '-'))
-      return result
+        result = self.__getJsonData("person", "/%s" % '-'.join(
+            args).lower().replace(' ', '-'))
+        return result
 
     def getFinancialOrgData(self, orgName):
-      '''This returns the data about a financial
-         organization in JSON format.'''
+        '''This returns the data about a financial
+           organization in JSON format.'''
 
-      result = self.__getJsonData("financial-organization", "/%s" % orgName)
-      return result
+        result = self.__getJsonData("financial-organization", "/%s" % orgName)
+        return result
 
     def getProductData(self, name):
-      '''This returns the data about a product in JSON format.'''
+        '''This returns the data about a product in JSON format.'''
 
-      result = self.__getJsonData("product", name)
-      return result
+        result = self.__getJsonData("product", name)
+        return result
 
     def getServiceProviderData(self, name):
-      '''This returns the data about a service provider in JSON format.'''
+        '''This returns the data about a service provider in JSON format.'''
 
-      result = self.__getJsonData("service-provider", "/%s" % name)
-      return result
+        result = self.__getJsonData("service-provider", "/%s" % name)
+        return result
 
     def listCompanies(self):
-      '''This returns the list of companies in JSON format.'''
+        '''This returns the list of companies in JSON format.'''
 
-      result = self.__getJsonData("companies")
-      return result
+        result = self.__getJsonData("companies")
+        return result
 
     def listPeople(self):
-      '''This returns the list of people in JSON format.'''
+        '''This returns the list of people in JSON format.'''
 
-      result = self.__getJsonData("people")
-      return result
+        result = self.__getJsonData("people")
+        return result
 
     def listFinancialOrgs(self):
-      '''This returns the list of financial organizations in JSON format.'''
+        '''This returns the list of financial organizations in JSON format.'''
 
-      result = self.__getJsonData("financial-organizations")
-      return result
+        result = self.__getJsonData("financial-organizations")
+        return result
 
     def listProducts(self):
-      '''This returns the list of products in JSON format.'''
+        '''This returns the list of products in JSON format.'''
 
-      result = self.__getJsonData("products")
-      return result
+        result = self.__getJsonData("products")
+        return result
 
     def listServiceProviders(self):
-      '''This returns the list of service providers in JSON format.'''
+        '''This returns the list of service providers in JSON format.'''
 
-      result = self.__getJsonData("service-providers")
-      return result
+        result = self.__getJsonData("service-providers")
+        return result
 
 
 class CrunchBaseResponse(object):
     def __init__(self, **kwargs):
-      self.__dict__.update(kwargs)
+        self.__dict__.update(kwargs)
 
     def __repr__(self):
-      return '%s(%r)' % (self.__class__.__name__, self.__dict__)
+        return '%s(%r)' % (self.__class__.__name__, self.__dict__)
 
 
 class CrunchBaseError(Exception):
